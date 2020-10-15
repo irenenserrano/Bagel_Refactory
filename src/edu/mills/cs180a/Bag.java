2b2995f678deab34cf069f2e3f0b81216beb3e83
Ellen Spertus <spertus@mills.edu>
2020-10-15 15:11:22 -0700

    Remove magic numbers

--- src/edu/mills/cs180a/Bag.java
+++ src/edu/mills/cs180a/Bag.java
@@ -7,76 +7,73 @@ package edu.mills.cs180a;
  *
  */
 public class Bag {
-    private final Bagel bagel;
-    private final int quantity;
+	// We provide a Baker's Dozen: 13 bagels for the price of 12.
+	private static final int BUY_ONE_GET_ONE_FREE_QUANTITY = 13;
 
-    /**
-     * Constructs a bag with the given quantity of the given type of bagel.
-     *
-     * @param bagel the type of bagel
-     * @param quantity the quantity
-     */
-    public Bag(Bagel bagel, int quantity) {
-        this.bagel = bagel;
-        this.quantity = quantity;
-    }
+	// If the Baker's Dozen discount doesn't apply, we give a percentage discount.
+	private static final int BULK_DISCOUNT_MINIMUM = 6;
+	private static final double BULK_DISCOUNT_PERCENTAGE = .05;
+	private static final double BULK_DISCOUNT_MULTIPLIER = 1 - BULK_DISCOUNT_PERCENTAGE;
 
-    /**
-     * Gets the bagel held in this bag.
-     *
-     * @return the bagel
-     */
-    public Bagel getBagel() {
-        return bagel;
-    }
+	private final Bagel bagel;
+	private final int quantity;
 
-    /**
-     * Gets the number of bagels in this bag.
-     *
-     * @return the number of bagels in this bag
-     */
-    public int getQuantity() {
-        return quantity;
-    }
+	/**
+	 * Constructs a bag with the given quantity of the given type of bagel.
+	 *
+	 * @param bagel    the type of bagel
+	 * @param quantity the quantity
+	 */
+	public Bag(Bagel bagel, int quantity) {
+		this.bagel = bagel;
+		this.quantity = quantity;
+	}
 
-    /**
-     * Gets the total price for this bag of bagels. This may be less than the per-bagel price times
-     * the number of bagels because of quantity discounts.
-     *
-     * @return the total price
-     */
-    public double getTotalPrice() {
-        double itemPrice;
-        if (bagel.getCategory().equals("old-fashioned")) {
-            itemPrice = .5; // 50 cents
-        } else if (bagel.getCategory().equals("gourmet")) {
-            itemPrice = .7; // 70 cents
-        } else if (bagel.getCategory().equals("discounted")) {
-            itemPrice = .35; // 35 cents
-        } else {
-            throw new IllegalArgumentException("Illegal category: " + bagel.getCategory());
-        }
-        double totalPrice;
-        if (quantity == 13) {
-            totalPrice = itemPrice * 12; // Baker's dozen
-        } else if (quantity >= 6) {
-            totalPrice = itemPrice * quantity * .95; // 5% discount
-        } else {
-            totalPrice = itemPrice * quantity;
-        }
-        return totalPrice;
-    }
+	/**
+	 * Gets the bagel held in this bag.
+	 *
+	 * @return the bagel
+	 */
+	public Bagel getBagel() {
+		return bagel;
+	}
 
-    // for student to write
-    public double getPerBagelPrice() {
-        if (bagel.getCategory().equals("old-fashioned")) {
-            return .5;
-        } else if (bagel.getCategory().equals("gourmet")) {
-            return .7;
-        } else if (bagel.getCategory().equals("discounted")) {
-            return .35;
-        } else {
-            throw new IllegalArgumentException("Illegal category");
-        }
-    }
+	/**
+	 * Gets the number of bagels in this bag.
+	 *
+	 * @return the number of bagels in this bag
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * Gets the total price for this bag of bagels. This may be less than the
+	 * per-bagel price times the number of bagels because of quantity discounts.
+	 *
+	 * @return the total price
+	 */
+	public double getTotalPrice() {
+		double undiscountedPrice = quantity * getPerBagelPrice();
+		if (quantity == BUY_ONE_GET_ONE_FREE_QUANTITY) {
+			return undiscountedPrice - getPerBagelPrice();
+		}
+		if (quantity >= BULK_DISCOUNT_MINIMUM) {
+			return undiscountedPrice * BULK_DISCOUNT_MULTIPLIER;
+		}
+		return undiscountedPrice;
+	}
+
+	// for student to write
+	public double getPerBagelPrice() {
+		if (bagel.getCategory().equals("old-fashioned")) {
+			return .5;
+		} else if (bagel.getCategory().equals("gourmet")) {
+			return .7;
+		} else if (bagel.getCategory().equals("discounted")) {
+			return .35;
+		} else {
+			throw new IllegalArgumentException("Illegal category");
+		}
+	}
 }
--- src/edu/mills/cs180a/Bagel.java
+++ src/edu/mills/cs180a/Bagel.java
@@ -3,60 +3,61 @@ package edu.mills.cs180a;
 import java.util.List;
 
 /**
- * A type of bagel. There should be only one instance of this class for any bagel type (e.g.,
- * "sesame seed").
+ * A type of bagel. There should be only one instance of this class for any
+ * bagel type (e.g., "sesame seed").
  *
  * @author Ellen Spertus
  */
 public class Bagel {
-    private static List<String> OLD_FASHIONED_TYPES =
-            List.of("plain", "poppy seed", "sesame seed", "onion", "everything");
-    private static List<String> GOURMET_TYPES =
-            List.of("asiago", "blueberry", "cinnamon raisin", "sun-dried tomato");
-    public static List<String> CATEGORIES = List.of("old-fashioned", "gourmet", "discounted");
-    private final String type;
-    private String category;
+	private static List<String> OLD_FASHIONED_TYPES = List.of("plain", "poppy seed", "sesame seed", "onion",
+			"everything");
+	private static List<String> GOURMET_TYPES = List.of("asiago", "blueberry", "cinnamon raisin", "sun-dried tomato");
+	public static List<String> CATEGORIES = List.of("old-fashioned", "gourmet", "day-old");
+	private final String type;
+	private String category;
 
-    /**
-     * Constructs a bagel of the given type.
-     *
-     * @param type one of "plain", "poppy seed", "sesame seed", "onion", "everything", "asiago",
-     *        "blueberry", "raisin", or "sun-dried tomato"
-     * @throws IllegalArgumentException if the type is not legal
-     */
-    public Bagel(String type) {
-        if (OLD_FASHIONED_TYPES.contains(type)) {
-            category = "old-fashioned";
-        } else if (GOURMET_TYPES.contains(type)) {
-            category = "gourmet";
-        } else {
-            throw new IllegalArgumentException("Illegal bagel type: " + type);
-        }
-        this.type = type;
-    }
+	/**
+	 * Constructs a bagel of the given type.
+	 *
+	 * @param type one of "plain", "poppy seed", "sesame seed", "onion",
+	 *             "everything", "asiago", "blueberry", "raisin", or "sun-dried
+	 *             tomato"
+	 * @throws IllegalArgumentException if the type is not legal
+	 */
+	public Bagel(String type) {
+		if (OLD_FASHIONED_TYPES.contains(type)) {
+			category = "old-fashioned";
+		} else if (GOURMET_TYPES.contains(type)) {
+			category = "gourmet";
+		} else {
+			throw new IllegalArgumentException("Illegal bagel type: " + type);
+		}
+		this.type = type;
+	}
 
-    /**
-     * Gets the type of the bagel.
-     *
-     * @return the type of bagel
-     */
-    public String getType() {
-        return type;
-    }
+	/**
+	 * Gets the type of the bagel.
+	 *
+	 * @return the type of bagel
+	 */
+	public String getType() {
+		return type;
+	}
 
-    /**
-     * Gets the category of the bagel. This is "old-fashioned", "gourmet", or "discounted".
-     *
-     * @return the category
-     */
-    public String getCategory() {
-        return category;
-    }
+	/**
+	 * Gets the category of the bagel. This is "old-fashioned", "gourmet", or
+	 * "discounted".
+	 *
+	 * @return the category
+	 */
+	public String getCategory() {
+		return category;
+	}
 
-    /**
-     * Discounts this bagel.
-     */
-    public void discount() {
-        category = "discounted";
-    }
+	/**
+	 * Marks down this bagel.
+	 */
+	public void markDown() {
+		category = "day-old";
+	}
 }

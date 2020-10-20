015d11d4323eb1f8eb70d5c9885fd29b10b2361d
iserrano <iserrano@mills.edu>
2020-10-20 13:26:15 -0700

    Replace all doubles with BigDecimal

--- src/edu/mills/cs180a/Bag.java
+++ src/edu/mills/cs180a/Bag.java
@@ -1,5 +1,7 @@
 package edu.mills.cs180a;
 
+import java.math.BigDecimal;
+
 /**
  * A bag of any quantity of a single type of bagel.
  *
@@ -7,73 +9,73 @@ package edu.mills.cs180a;
  *
  */
 public class Bag {
-    // We provide a Baker's Dozen: 13 bagels for the price of 12.
-    private static final int BUY_ONE_GET_ONE_FREE_QUANTITY = 13;
+	// We provide a Baker's Dozen: 13 bagels for the price of 12.
+	private static final int BUY_ONE_GET_ONE_FREE_QUANTITY = 13;
 
-    // If the Baker's Dozen discount doesn't apply, we give a percentage discount.
-    private static final int BULK_DISCOUNT_MINIMUM = 6;
-    private static final double BULK_DISCOUNT_PERCENTAGE = .05;
-    private static final double BULK_DISCOUNT_MULTIPLIER = 1 - BULK_DISCOUNT_PERCENTAGE;
+	// If the Baker's Dozen discount doesn't apply, we give a percentage discount.
+	private static final int BULK_DISCOUNT_MINIMUM = 6;
+	private static final BigDecimal BULK_DISCOUNT_PERCENTAGE = BigDecimal.valueOf(.05);
+	private static final BigDecimal BULK_DISCOUNT_MULTIPLIER = BigDecimal.valueOf(1).subtract(BULK_DISCOUNT_PERCENTAGE);
 
-    private final Bagel bagel;
-    private final int quantity;
+	private final Bagel bagel;
+	private final int quantity;
 
-    /**
-     * Constructs a bag with the given quantity of the given type of bagel.
-     *
-     * @param bagel the type of bagel
-     * @param quantity the quantity
-     */
-    public Bag(Bagel bagel, int quantity) {
-        this.bagel = bagel;
-        this.quantity = quantity;
-    }
+	/**
+	 * Constructs a bag with the given quantity of the given type of bagel.
+	 *
+	 * @param bagel the type of bagel
+	 * @param quantity the quantity
+	 */
+	public Bag(Bagel bagel, int quantity) {
+		this.bagel = bagel;
+		this.quantity = quantity;
+	}
 
-    /**
-     * Gets the bagel held in this bag.
-     *
-     * @return the bagel
-     */
-    public Bagel getBagel() {
-        return bagel;
-    }
+	/**
+	 * Gets the bagel held in this bag.
+	 *
+	 * @return the bagel
+	 */
+	public Bagel getBagel() {
+		return bagel;
+	}
 
-    /**
-     * Gets the number of bagels in this bag.
-     *
-     * @return the number of bagels in this bag
-     */
-    public int getQuantity() {
-        return quantity;
-    }
+	/**
+	 * Gets the number of bagels in this bag.
+	 *
+	 * @return the number of bagels in this bag
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
 
-    /**
-     * Gets the total price for this bag of bagels. This may be less than the per-bagel price times
-     * the number of bagels because of quantity discounts.
-     *
-     * @return the total price
-     */
-    public double getTotalPrice() {
-        double undiscountedPrice = quantity * getPerBagelPrice();
-        if (quantity == BUY_ONE_GET_ONE_FREE_QUANTITY) {
-            return undiscountedPrice - getPerBagelPrice();
-        }
-        if (quantity >= BULK_DISCOUNT_MINIMUM) {
-            return undiscountedPrice * BULK_DISCOUNT_MULTIPLIER;
-        }
-        return undiscountedPrice;
-    }
+	/**
+	 * Gets the total price for this bag of bagels. This may be less than the per-bagel price times
+	 * the number of bagels because of quantity discounts.
+	 *
+	 * @return the total price
+	 */
+	public BigDecimal getTotalPrice() {
+		BigDecimal undiscountedPrice = getPerBagelPrice().multiply(BigDecimal.valueOf(quantity));
+		if (quantity == BUY_ONE_GET_ONE_FREE_QUANTITY) {
+			return undiscountedPrice.subtract(getPerBagelPrice());
+		}
+		if (quantity >= BULK_DISCOUNT_MINIMUM) {
+			return undiscountedPrice.multiply(BULK_DISCOUNT_MULTIPLIER);
+		}
+		return undiscountedPrice;
+	}
 
-    /**
-     * Returns the price associated with the bagel type.
-     *
-     * @return price based on bagel type
-     */
-    public double getPerBagelPrice() {
-        return bagel.getCategory().getPrice();
+	/**
+	 * Returns the price associated with the bagel type.
+	 *
+	 * @return price based on bagel type
+	 */
+	public BigDecimal getPerBagelPrice() {
+		return bagel.getCategory().getPrice();
 
-        //or you could do
-        //Category category = bagel.getCategory();
-        //return category.getPrice();
-    }
+		//or you could do
+		//Category category = bagel.getCategory();
+		//return category.getPrice();
+	}
 }
--- src/edu/mills/cs180a/Bagel.java
+++ src/edu/mills/cs180a/Bagel.java
@@ -1,87 +1,89 @@
 package edu.mills.cs180a;
 
+import java.math.BigDecimal;
+
 /**
  * A type of bagel.
  *
  * @author Ellen Spertus
  */
 public class Bagel {
-    private final Type type;
-    private Category currentCategory;
+	private final Type type;
+	private Category currentCategory;
 
-    enum Type {
-        PLAIN("plain", Category.OLD_FASHIONED), POPPY_SEED("poppy seed",
-                Category.OLD_FASHIONED), SESAME_SEED("sesame seed", Category.OLD_FASHIONED), ONION(
-                        "onion",Category.OLD_FASHIONED), EVERYTHING("everthing",
-                                Category.OLD_FASHIONED), ASIAGO("asiago",
-                                        Category.GOURMET), BLUEBERRY("blueberry",
-                                                Category.GOURMET), CINNAMON_RAISIN(
-                                                        "cinnamon raisin",
-                                                        Category.GOURMET), SUN_DRIED_TOMATO(
-                                                                "sun dried tomato",
-                                                                Category.GOURMET);
+	enum Type {
+		PLAIN("plain", Category.OLD_FASHIONED), POPPY_SEED("poppy seed",
+				Category.OLD_FASHIONED), SESAME_SEED("sesame seed", Category.OLD_FASHIONED), ONION(
+						"onion",Category.OLD_FASHIONED), EVERYTHING("everthing",
+								Category.OLD_FASHIONED), ASIAGO("asiago",
+										Category.GOURMET), BLUEBERRY("blueberry",
+												Category.GOURMET), CINNAMON_RAISIN(
+														"cinnamon raisin",
+														Category.GOURMET), SUN_DRIED_TOMATO(
+																"sun dried tomato",
+																Category.GOURMET);
 
-        private final String name;
-        private final Category category;
+		private final String name;
+		private final Category category;
 
-        private Type(String name, Category category) {
-            this.name = name;
-            this.category = category;
-        }
+		private Type(String name, Category category) {
+			this.name = name;
+			this.category = category;
+		}
 
-        @Override
-        public String toString() {
-            return name;
-        }
+		@Override
+		public String toString() {
+			return name;
+		}
 
-    }
+	}
 
-    enum Category {
-        OLD_FASHIONED(.5), GOURMET(.7), DAY_OLD(.35);
+	enum Category {
+		OLD_FASHIONED(BigDecimal.valueOf(.5)), GOURMET(BigDecimal.valueOf(.7)), DAY_OLD(BigDecimal.valueOf(.35));
 
-        private final double price;
+		private final BigDecimal price;
 
-        private Category(double price) {
-            this.price = price;
-        }
+		private Category(BigDecimal price) {
+			this.price = price;
+		}
 
-        public double getPrice() {
-            return price;
-        }
-    }
+		public BigDecimal getPrice() {
+			return price;
+		}
+	}
 
-    /**
-     * Constructs a bagel of the given type.
-     *
-     * @param the type
-     */
-    public Bagel(Type type) {
-        this.type = type;
-        this.currentCategory = type.category;
-    }
+	/**
+	 * Constructs a bagel of the given type.
+	 *
+	 * @param the type
+	 */
+	public Bagel(Type type) {
+		this.type = type;
+		this.currentCategory = type.category;
+	}
 
-    /**
-     * Gets the type of the bagel.
-     *
-     * @return the type of bagel
-     */
-    public Type getType() {
-        return type;
-    }
+	/**
+	 * Gets the type of the bagel.
+	 *
+	 * @return the type of bagel
+	 */
+	public Type getType() {
+		return type;
+	}
 
-    /**
-     * Gets the category of the bagel.
-     *
-     * @return the category
-     */
-    public Category getCategory() {
-        return currentCategory;
-    }
+	/**
+	 * Gets the category of the bagel.
+	 *
+	 * @return the category
+	 */
+	public Category getCategory() {
+		return currentCategory;
+	}
 
-    /**
-     * Marks down this bagel.
-     */
-    public void markDown() {
-        currentCategory = Category.DAY_OLD;
-    }
+	/**
+	 * Marks down this bagel.
+	 */
+	public void markDown() {
+		currentCategory = Category.DAY_OLD;
+	}
 }

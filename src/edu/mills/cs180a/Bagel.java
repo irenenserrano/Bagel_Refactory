06bd01ebaf14fede3a23f9f3cb8f19536e9e4891
iserrano <iserrano@mills.edu>
2020-10-22 13:03:14 -0700

    Add toString methods

--- src/edu/mills/cs180a/Bagel.java
+++ src/edu/mills/cs180a/Bagel.java
@@ -8,87 +8,93 @@ import java.math.BigDecimal;
  * @author Ellen Spertus
  */
 public class Bagel {
-	private final Type type;
-	private Category currentCategory;
-	// Visible for Testing
-	protected static final BigDecimal OLD_FASHIONED_PRICE = BigDecimal.valueOf(0.5);
-	protected static final BigDecimal GOURMET_PRICE = BigDecimal.valueOf(0.7);
-	protected static final BigDecimal DAY_OLD_PRICE= BigDecimal.valueOf(0.35);
+    private final Type type;
+    private Category currentCategory;
+    // Visible for Testing
+    protected static final BigDecimal OLD_FASHIONED_PRICE = BigDecimal.valueOf(0.5);
+    protected static final BigDecimal GOURMET_PRICE = BigDecimal.valueOf(0.7);
+    protected static final BigDecimal DAY_OLD_PRICE= BigDecimal.valueOf(0.35);
 
 
-	enum Type {
-		PLAIN("plain", Category.OLD_FASHIONED), POPPY_SEED("poppy seed",
-				Category.OLD_FASHIONED), SESAME_SEED("sesame seed", Category.OLD_FASHIONED), ONION(
-						"onion",Category.OLD_FASHIONED), EVERYTHING("everthing",
-								Category.OLD_FASHIONED), ASIAGO("asiago",
-										Category.GOURMET), BLUEBERRY("blueberry",
-												Category.GOURMET), CINNAMON_RAISIN(
-														"cinnamon raisin",
-														Category.GOURMET), SUN_DRIED_TOMATO(
-																"sun dried tomato",
-																Category.GOURMET);
+    enum Type {
+        PLAIN("plain", Category.OLD_FASHIONED), POPPY_SEED("poppy seed",
+                Category.OLD_FASHIONED), SESAME_SEED("sesame seed", Category.OLD_FASHIONED), ONION(
+                        "onion",Category.OLD_FASHIONED), EVERYTHING("everthing",
+                                Category.OLD_FASHIONED), ASIAGO("asiago",
+                                        Category.GOURMET), BLUEBERRY("blueberry",
+                                                Category.GOURMET), CINNAMON_RAISIN(
+                                                        "cinnamon raisin",
+                                                        Category.GOURMET), SUN_DRIED_TOMATO(
+                                                                "sun dried tomato",
+                                                                Category.GOURMET);
 
-		private final String name;
-		private final Category category;
+        private final String name;
+        private final Category category;
 
-		private Type(String name, Category category) {
-			this.name = name;
-			this.category = category;
-		}
+        private Type(String name, Category category) {
+            this.name = name;
+            this.category = category;
+        }
 
-		@Override
-		public String toString() {
-			return name;
-		}
+        @Override
+        public String toString() {
+            return name;
+        }
 
-	}
+    }
 
-	enum Category {
-		OLD_FASHIONED(OLD_FASHIONED_PRICE), GOURMET(GOURMET_PRICE), DAY_OLD(DAY_OLD_PRICE);
+    enum Category {
+        OLD_FASHIONED(OLD_FASHIONED_PRICE), GOURMET(GOURMET_PRICE), DAY_OLD(DAY_OLD_PRICE);
 
-		private final BigDecimal price;
+        private final BigDecimal price;
 
-		private Category(BigDecimal price) {
-			this.price = price;
-		}
+        private Category(BigDecimal price) {
+            this.price = price;
+        }
 
-		public BigDecimal getPrice() {
-			return price;
-		}
-	}
+        public BigDecimal getPrice() {
+            return price;
+        }
+    }
 
-	/**
-	 * Constructs a bagel of the given type.
-	 *
-	 * @param the type
-	 */
-	public Bagel(Type type) {
-		this.type = type;
-		this.currentCategory = type.category;
-	}
+    /**
+     * Constructs a bagel of the given type.
+     *
+     * @param the type
+     */
+    public Bagel(Type type) {
+        this.type = type;
+        this.currentCategory = type.category;
+    }
 
-	/**
-	 * Gets the type of the bagel.
-	 *
-	 * @return the type of bagel
-	 */
-	public Type getType() {
-		return type;
-	}
+    /**
+     * Gets the type of the bagel.
+     *
+     * @return the type of bagel
+     */
+    public Type getType() {
+        return type;
+    }
 
-	/**
-	 * Gets the category of the bagel.
-	 *
-	 * @return the category
-	 */
-	public Category getCategory() {
-		return currentCategory;
-	}
+    /**
+     * Gets the category of the bagel.
+     *
+     * @return the category
+     */
+    public Category getCategory() {
+        return currentCategory;
+    }
 
-	/**
-	 * Marks down this bagel.
-	 */
-	public void markDown() {
-		currentCategory = Category.DAY_OLD;
-	}
+    /**
+     * Marks down this bagel.
+     */
+    public void markDown() {
+        currentCategory = Category.DAY_OLD;
+    }
+
+    @Override
+    public String toString() {
+        return type.toString();
+    }
+
 }
--- src/edu/mills/cs180a/Order.java
+++ src/edu/mills/cs180a/Order.java
@@ -10,81 +10,91 @@ import java.util.List;
  * @author Ellen Spertus
  */
 public class Order {
-	private final List<Bag> bags;
+    private final List<Bag> bags;
 
-	/**
-	 * Constructs an order with a single bag of bagels.
-	 *
-	 * @param bag the bag
-	 */
-	public Order(Bag bag) {
-		bags = List.of(bag);
-	}
+    /**
+     * Constructs an order with a single bag of bagels.
+     *
+     * @param bag the bag
+     */
+    public Order(Bag bag) {
+        bags = List.of(bag);
+    }
 
-	/**
-	 * Constructs an order with two bags of bagels.
-	 *
-	 * @param bag1 the first bag
-	 * @param bag2 the second bag
-	 */
-	public Order(Bag bag1, Bag bag2) {
-		bags = List.of(bag1, bag2);
-	}
+    /**
+     * Constructs an order with two bags of bagels.
+     *
+     * @param bag1 the first bag
+     * @param bag2 the second bag
+     */
+    public Order(Bag bag1, Bag bag2) {
+        bags = List.of(bag1, bag2);
+    }
 
-	/**
-	 * Constructs an order with three bags of bagels.
-	 *
-	 * @param bag1 the first bag
-	 * @param bag2 the second bag
-	 * @param bag3 the third bag
-	 */
-	public Order(Bag bag1, Bag bag2, Bag bag3) {
-		bags = List.of(bag1, bag2, bag3);
-	}
+    /**
+     * Constructs an order with three bags of bagels.
+     *
+     * @param bag1 the first bag
+     * @param bag2 the second bag
+     * @param bag3 the third bag
+     */
+    public Order(Bag bag1, Bag bag2, Bag bag3) {
+        bags = List.of(bag1, bag2, bag3);
+    }
 
-	/**
-	 * Constructs an order with the specifies bags
-	 *
-	 * @param bags
-	 */
-	public Order(Bag... bags) {
-		this.bags = Arrays.asList(bags);
-	}
+    /**
+     * Constructs an order with the specifies bags
+     *
+     * @param bags
+     */
+    public Order(Bag... bags) {
+        this.bags = Arrays.asList(bags);
+    }
 
-	/**
-	 * Generates the total price of a single order based on the contents of each bag
-	 *
-	 * @return price
-	 */
-	public BigDecimal getPrice() {
-		BigDecimal price = BigDecimal.valueOf(0);
-		for (Bag bag : bags) {
-			price = price.add(bag.getTotalPrice());
-		}
-		return price;
-	}
+    /**
+     * Generates the total price of a single order based on the contents of each bag
+     *
+     * @return price
+     */
+    public BigDecimal getPrice() {
+        BigDecimal price = BigDecimal.valueOf(0);
+        for (Bag bag : bags) {
+            price = price.add(bag.getTotalPrice());
+        }
+        return price;
+    }
 
-	/**
-	 * Generates a printed receipt containing the contents, discounts, and total
-	 * final price
-	 *
-	 * @return s Generated receipt of order
-	 */
-	public String generateReceipt() {
-		String s = "";
-		BigDecimal total = BigDecimal.valueOf(0);
-		for (Bag bag : bags) {
-			total = total.add(bag.getTotalPrice());
-			s += bag.getBagel().getType() + "\n" + "\tquantity: " + bag.getQuantity() + "\n" + "\tprice each: $"
-					+ bag.getPerBagelPrice() + "\n" + "\total: $" + "\ttotal: " + bag.getTotalPrice() + "\n";
-			if (bag.getTotalPrice()
-					.compareTo(bag.getPerBagelPrice().multiply(BigDecimal.valueOf(bag.getQuantity()))) < 0) {
-				s += "\tYou saved $" + (bag.getPerBagelPrice().multiply(BigDecimal.valueOf(bag.getQuantity()))
-						.subtract(bag.getTotalPrice())) + " through our volume discount program.\n";
-			}
-		}
-		s += "TOTAL: $" + total + "\n";
-		s += "Thank you for shopping at the Bagel Refactory!\n";
-		return s;
-	}
+    /**
+     * Generates a printed receipt containing the contents, discounts, and total final price
+     *
+     * @return s Generated receipt of order
+     */
+    public String generateReceipt() {
+        String s = "";
+        BigDecimal total = BigDecimal.valueOf(0);
+        for (Bag bag : bags) {
+            total = total.add(bag.getTotalPrice());
+            s += bag.getBagel().getType() + "\n" + "\tquantity: " + bag.getQuantity() + "\n"
+                    + "\tprice each: $"
+                    + bag.getPerBagelPrice() + "\n" + "\total: $" + "\ttotal: "
+                    + bag.getTotalPrice() + "\n";
+            if (bag.getTotalPrice()
+                    .compareTo(bag.getPerBagelPrice()
+                            .multiply(BigDecimal.valueOf(bag.getQuantity()))) < 0) {
+                s += "\tYou saved $"
+                        + (bag.getPerBagelPrice().multiply(BigDecimal.valueOf(bag.getQuantity()))
+                                .subtract(bag.getTotalPrice()))
+                        + " through our volume discount program.\n";
+            }
+        }
+        s += "TOTAL: $" + total + "\n";
+        s += "Thank you for shopping at the Bagel Refactory!\n";
+        return s;
+    }
+
+    @Override
+    public String toString() {
+        return bags.toString();
+    }
+
 }
